c2c8fd646766ac344f07bd7230dafeab33a06a2d
Ian Chan <yingchungchan@yahoo.com.hk>
2018-12-19 12:18:06 +0800

    feat(main): refactoring by function call, move the items by a dictionary can do some random later

--- main.py
+++ main.py
@@ -2,86 +2,102 @@ import fut
 import datetime
 import time
 import os
+import random
 
 first_time = True
-asset_id = 5003076
-buy_now_price = 1500
-max_price = 1500
-sell_min_price = 1800
-sell_max_price = 1900
+min_margin = 500
+
+items = [
+  {'ctype': 'training', 'asset_id': 5003076, 'buy_in': 1500}
+]
+
+def search_and_buy(session, item):
+  print('Going to search and buy...')
+  results = session.search(ctype=item['ctype'],assetId=item['asset_id'],page_size=5,max_buy=item['buy_in'])
+  print('Buy now count: ', len(results))
+  for result in results:
+    print('Going to buy now...')
+    session.bid(result['tradeId'], item['buy_in'], fast=True)
+
+
+def search_and_bid(session, item):
+  print('Going to search and bid...')
+  results = session.search(ctype=item['ctype'],assetId=item['asset_id'],page_size=5,max_price=item['buy_in'])
+  print('Bid count: ', len(results), ' and check if within 1 min')
+  for result in results:
+    if result['expires'] <= 60:
+      print('Going to bid now...')
+      session.bid(result['tradeId'], item['buy_in'], fast=True)
+
+def clean_watchlist(session):
+  watchlist = session.watchlist()
+  print('Watchlist Count:', len(watchlist))
+  for result in watchlist:
+    if result['bidState'] == 'outbid':
+      session.watchlistDelete(result['tradeId'])
+      print('Cleaning watchlist...')
+
+    if result['bidState'] == 'highest':
+      session.sendToTradepile(result['id'])
+
+def clean_tradepile(session):
+  relist = False
+  clear = False
+  print('Check the tradepile again...')
+  for result in session.tradepile():
+    if result['tradeState'] == None:
+      for item in items:
+        if result['resourceId'] == item['asset_id']:
+          session.sell(result['id'], bid=item['buy_in']+min_margin, buy_now=item['buy_in']+min_margin+100)
+
+    if result['tradeState'] == 'expired':
+      relist = True
+
+    if result['tradeState'] == 'closed':
+      clear = True
+
+  if relist == True:
+    print('Relist some items')
+    session.relist()
+
+  if clear == True:
+    print('Clear sold items')
+    session.tradepileClear()
+
 
 while True:
   try:
     if first_time == True:
       print('Welcome to FUT!')
-      print('Login: ',datetime.datetime.now()) 
+      print('Login: ', datetime.datetime.now()) 
       session = fut.Core(os.environ['email'], os.environ['password'], os.environ['secret'], platform="ps4")
       first_time = False
+      print('Successfully login at ', datetime.datetime.now())
 
     session.keepalive()
     tradepile_count = len(session.tradepile())
     print('Tradepile count:', tradepile_count)
 
     if tradepile_count >= 95:
-      print('tradepile nearly full...')
+      print('Tradepile nearly full...')
     else:
-      print('going to search...')
-      results = session.search(ctype='training',category='position',assetId=asset_id,page_size=50,max_buy=buy_now_price)
-      print('buy now count', len(results))
-      for result in results:
-        print('going buy now...')
-        session.bid(result['tradeId'], buy_now_price, fast=True)
-        # session.sendToTradepile(result['id'])
+      search_and_buy(session, items[0])
+      time.sleep(1)
 
+      search_and_bid(session, items[0])
+      time.sleep(1)
 
-      results = session.search(ctype='training',category='position',assetId=asset_id,page_size=10,max_price=max_price)
-      print('max price but maybe take longer time, count:', len(results))
-      for result in results:
-        if result['expires'] <= 60:
-          print('going to bid now...')
-          session.bid(result['tradeId'], max_price+100, fast=True)
-          time.sleep(0.5)
+      clean_watchlist(session)
+      time.sleep(1)
 
-      watchlist = session.watchlist()
-      print('Watchlist Count:', len(watchlist))
-      for result in watchlist:
-        if result['bidState'] == 'outbid':
-          session.watchlistDelete(result['tradeId'])
-          print('cleaning watchlist...')
-          time.sleep(0.5)
-
-        if result['bidState'] == 'highest':
-          session.sendToTradepile(result['id'])
-
-
-    relist = False
-    clear = False
-    print('Check the tradepile again...')
-    for result in session.tradepile():
-      if result['tradeState'] == None:
-        session.sell(result['id'], bid=sell_min_price, buy_now=sell_max_price)
-
-      if result['tradeState'] == 'expired':
-        relist = True
-
-      if result['tradeState'] == 'closed':
-        clear = True
-
-    if relist == True:
-      print('Relist some items')
-      session.relist()
-
-    if clear == True:
-      print('Clear sold items')
-      session.tradepileClear()
-
+    clean_tradepile(session)
     print('Time now is : ',datetime.datetime.now())
+    print('----------------------------------------')
     time.sleep(5)
   except NameError:
     print('*** Session dropped ***')
-    print('Login: ',datetime.datetime.now()) 
+    print(NameError)
+    print('Login again: ',datetime.datetime.now())
     session = fut.Core(os.environ['email'], os.environ['password'], os.environ['secret'], platform="ps4")
-  # else:
-    # print('Else case')
-  # finally:
-    # print('Must go through')
+
+
